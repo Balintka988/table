2cc570bc377d4fcd7d799b7ee69123b23e5e504e
Szikora Balint <balint.szikora00@gmail.com>
2024-10-16 17:58:55 +0200

    táblázat sor hozzáadás

--- main.js
+++ main.js
@@ -40,12 +40,18 @@ thead.appendChild(tr);
 const th_lastname = document.createElement('th');
 tr.appendChild(th_lastname);
 th_lastname.innerHTML='vezetéknév';
- 
-const th_firstname = document.createElement('th');
-tr.appendChild(th_firstname);
-th_firstname.innerHTML='keresztnév';
- 
-th_lastname.colSpan=2;
+
+const th_firstname1 = document.createElement('th');
+tr.appendChild(th_firstname1);
+th_firstname1.innerHTML='keresztnév1';
+
+const th_firstname2 = document.createElement('th');
+tr.appendChild(th_firstname2);
+th_firstname2.innerHTML='keresztnév2';
+
+
+th_firstname1.colSpan=2;
+
 
 const th_pet = document.createElement('th');
 tr.appendChild(th_pet);
@@ -58,62 +64,11 @@ th_married.innerHTML="hazas";
  
 const tbody = document.createElement('tbody');
 table.appendChild(tbody);
- 
-for(const pers of array){
-    const tbody_tr = document.createElement('tr');
-    tbody.appendChild(tbody_tr);
- 
-    const tbody_td_lastname = document.createElement('td');
-    tbody_tr.appendChild(tbody_td_lastname);
-   
-    tbody_td_lastname.innerHTML = pers.lastname;
- 
-    const tbody_td_firstname = document.createElement('td');
-    tbody_tr.appendChild(tbody_td_firstname);
-   
-    tbody_td_firstname.innerHTML = pers.firstname1;
 
-    
- 
-    if(pers.firstname2 === undefined){
-        tbody_td_firstname.colSpan = 2
-    }
-    else{
-        const tbody_td_firstname = document.createElement('td');
-        tbody_tr.appendChild(tbody_td_firstname);
-       
-        tbody_td_firstname.innerHTML = pers.firstname2;
-    }
-tbody_tr.addEventListener ('click', function(e){
-    //console.log('clicked');
-    const selected = tbody.querySelector('.selected')
-    
-    if (selected != undefined){
-        selected.classList.remove('selected');
-        
-    }
-    e.currentTarget.classList.add('selected')
-})
-
-const td_pet = document.createElement('td');
-    tbody_tr.appendChild(td_pet);
-    td_pet.innerHTML = pers.pet;
-
-    const td_married = document.createElement('td');
-    tbody_tr.appendChild(td_married);
-    td_married.innerHTML = pers.married;
-
-
-    if(pers.married === true){
-        td_married.innerHTML = "Igen"
-    }
-    else{
-        td_married.innerHTML = "Nem"
-    }
-}
 
 const form = document.getElementById('form');
 form.addEventListener('submit', function(e){
+    e.preventDefault()
     const lastname = document.getElementById('lastname');
     const firstname1 = document.getElementById('firstname1');
     const firstname2 = document.getElementById('firstname2');
@@ -122,7 +77,103 @@ form.addEventListener('submit', function(e){
 
     const lastnamevalue = lastname.value;
     const firstname1value = firstname1.value;
-    const firstname2value = firstname2.value;
+    let firstname2value = firstname2.value;
     const marriedvalue = married.checked;
     const petvalue = pet.value;
-});
+
+    if(validateFields(lastname, firstname1, pet)){
+        if(firstname2value === ""){
+            firstname2value = undefined;
+        }
+        const newperson = {
+            firstname1: firstname1value,
+            firstname2: firstname2value,
+            lastname: lastnamevalue,
+            married: marriedvalue,
+            pet: petvalue,
+        }
+        array.push(newperson);
+        console.log(array);
+        renderTable();
+    }
+})  
+
+renderTable();
+
+function renderTable(){
+    tbody.innerHTML = '';
+    for(const pers of array){
+        const tbody_tr = document.createElement('tr');
+        tbody.appendChild(tbody_tr);
+     
+        const tbody_td_lastname = document.createElement('td');
+        tbody_tr.appendChild(tbody_td_lastname);
+       
+        tbody_td_lastname.innerHTML = pers.lastname;
+     
+        const tbody_td_firstname = document.createElement('td');
+        tbody_tr.appendChild(tbody_td_firstname);
+       
+        tbody_td_firstname.innerHTML = pers.firstname1;
+    
+        
+        if(pers.firstname2 === undefined){
+            tbody_td_firstname.colSpan = 2
+        }
+        else{
+            const tbody_td_firstname = document.createElement('td');
+            tbody_tr.appendChild(tbody_td_firstname);
+           
+            tbody_td_firstname.innerHTML = pers.firstname2;
+        }
+
+        const td_pet = document.createElement('td');
+        tbody_tr.appendChild(td_pet);
+        td_pet.innerHTML = pers.pet;
+    
+        const td_married = document.createElement('td');
+        tbody_tr.appendChild(td_married);
+        td_married.innerHTML = pers.married;
+
+        
+        tbody_tr.addEventListener ('click', function(e){//console.log('clicked');
+        const selected = tbody.querySelector('.selected')
+        
+        if (selected != undefined){
+            selected.classList.remove('selected');
+            
+        }
+        e.currentTarget.classList.add('selected')
+    })
+
+        if(pers.married === true){
+            td_married.innerHTML = "Igen"
+        }
+        else{
+            td_married.innerHTML = "Nem"
+        }
+    }
+}
+
+function validateFields(lastname, firstname1, pet){
+    let result = true;
+    if(lastname.value === ""){
+        const apa = lastname.parentElement;
+        const error = apa.querySelector('.error')
+        error.innerHTML = "Kötelező"
+        result = false;
+    }
+    if(firstname1.value === ""){
+        const apa = firstname1.parentElement;
+        const error = apa.querySelector('.error')
+        error.innerHTML = 'Kötelező'
+        result = false;
+    }
+    if(pet.value === ""){
+        const apa = pet.parentElement;
+        const error = apa.querySelector('.error')
+        error.innerHTML = 'Kötelező'
+        result = false;
+    }
+    return result;
+}
